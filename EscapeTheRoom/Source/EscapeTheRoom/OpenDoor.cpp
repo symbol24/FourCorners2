9b908d536d0889ec272ca4b0fac045962bef027c
Unknown <phillip.conte@gmail.com>
2018-07-11 18:05:40 -0400

    Added door and opened it

--- EscapeTheRoom/Source/EscapeTheRoom/OpenDoor.cpp
+++ EscapeTheRoom/Source/EscapeTheRoom/OpenDoor.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReport.h"
+#include "OpenDoor.h"
 #include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -16,20 +16,21 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...	
-
-	FString ObjectPos = *GetOwner()->GetActorLocation().ToString();
-	UE_LOG(LogTemp, Error, TEXT("Position report reporting for duty from %s at position: %s"), *GetOwner()->GetName(), *ObjectPos);
+	
+	auto* Owner = GetOwner();
+	FRotator currentRotation = Owner->GetActorRotation();
+	FRotator newRoration = currentRotation.Add(0, -45, 0);
+	Owner->SetActorRotation(newRoration.Quaternion());
+	// ...
 	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
